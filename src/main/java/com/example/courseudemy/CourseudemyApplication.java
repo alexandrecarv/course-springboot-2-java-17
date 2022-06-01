f38b3b5e883e99910a0d35bbea2d7740a41ce974
Alexandre Carvalho <falexandreco@gmail.com>
2022-06-01 17:05:31 -0300

    Project Created

---
+++ src/main/java/com/example/courseudemy/CourseudemyApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseudemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseudemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/courseudemy/CourseudemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseudemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
